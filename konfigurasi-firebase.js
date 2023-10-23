4c596c7375030f73b55702717f53029a8da55b28
yaspiez56@gmail.com <yaspiez56@gmail.com>
2023-10-23 10:32:57 +0700

    update konfigurasi-firebase.js

--- konfigurasi-firebase.js
+++ konfigurasi-firebase.js
@@ -1,9 +1,9 @@
 export const konfigurasiFirebase = {
-    apiKey: "AIzaSyCjXlgysJkN-2s3Gu0forgp7as5-9NqCkI",
-    authDomain: "pasar-b04a7.firebaseapp.com",
-    projectId: "pasar-b04a7",
-    storageBucket: "pasar-b04a7.appspot.com",
-    messagingSenderId: "508470916587",
-    appId: "1:508470916587:web:460e9a1612e92b712e15ae",
-    measurementId: "G-33T7CQCWBX"
-  };
+apiKey: "AIzaSyAfc3wksCdytqvqxVnVyBQYckTJiP3ZXaA",
+  authDomain: "pasarcemerlang-9610a.firebaseapp.com",
+  projectId: "pasarcemerlang-9610a",
+  storageBucket: "pasarcemerlang-9610a.appspot.com",
+  messagingSenderId: "595222147019",
+  appId: "1:595222147019:web:c3770f9d87b39a6de4d04e",
+  measurementId: "G-GERW838MCP"
+};
