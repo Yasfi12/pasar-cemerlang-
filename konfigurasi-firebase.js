3d40e864dd55a5986a54f0eddc9190eebe74a213
yandiabdur@gmail.com <yandiabdur@gmail.com>
2023-10-23 09:59:57 +0700

    pasaronline

---
+++ konfigurasi-firebase.js
@@ -0,0 +1,9 @@
+export const konfigurasiFirebase = {
+    apiKey: "AIzaSyCjXlgysJkN-2s3Gu0forgp7as5-9NqCkI",
+    authDomain: "pasar-b04a7.firebaseapp.com",
+    projectId: "pasar-b04a7",
+    storageBucket: "pasar-b04a7.appspot.com",
+    messagingSenderId: "508470916587",
+    appId: "1:508470916587:web:460e9a1612e92b712e15ae",
+    measurementId: "G-33T7CQCWBX"
+  };
